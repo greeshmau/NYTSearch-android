3828eef7b84aa207222ba80d55f93c99cb8ce713
greeshmau <greeshmakashyap22@gmail.com>
2017-09-19 16:54:51 -0700

    Got searchView to work

--- app/src/main/java/com/codepath/gumapathi/nytsearch/ListActivity.java
+++ app/src/main/java/com/codepath/gumapathi/nytsearch/ListActivity.java
@@ -1,15 +1,17 @@
 package com.codepath.gumapathi.nytsearch;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.view.ActionMode;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.codepath.gumapathi.nytsearch.Adapter.ArticlesAdapter;
@@ -27,108 +29,106 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+//import android.support.v7.widget.
+
 
 public class ListActivity extends AppCompatActivity {
     final Gson gson = new Gson();
     ArticlesAdapter articlesAdapter;
-    private EndlessRecyclerViewScrollListener scrollListener;
     ArrayList<Doc> allArticles;
-
-    // Tracks current contextual action mode
-    private ActionMode currentActionMode;
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         /*
-         * Menu/Action Item related stuff
-         */
-
-
-        /*
          * Toolbar related stuff
          */
-        {
-            // Find the toolbar view inside the activity layout
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            // Sets the Toolbar to act as the ActionBar for this Activity window.
-            // Make sure the toolbar exists in the activity and is not null
-            setSupportActionBar(toolbar);
-        }
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        // Find the toolbar view and set as ActionBar
+        // ...
+        // Display icon in the toolbar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
 
         /*
          * Recycleview related stuff
          */
-        {
-            RecyclerView rvArticles = (RecyclerView) findViewById(R.id.rvArticles);
-            allArticles = new ArrayList<>();
-            StaggeredGridLayoutManager staggeredGridLayoutManager =
-                    new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-            staggeredGridLayoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
 
-            // Attach the layout manager to the recycler view
-            rvArticles.setLayoutManager(staggeredGridLayoutManager);
-            articlesAdapter = new ArticlesAdapter(allArticles);
-            rvArticles.setAdapter(articlesAdapter);
+        RecyclerView rvArticles = (RecyclerView) findViewById(R.id.rvArticles);
+        allArticles = new ArrayList<>();
+        StaggeredGridLayoutManager staggeredGridLayoutManager =
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        staggeredGridLayoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
 
-            scrollListener = new EndlessRecyclerViewScrollListener(staggeredGridLayoutManager) {
-                @Override
-                public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                    // Triggered only when new data needs to be appended to the list
-                    // Add whatever code is needed to append new items to the bottom of the list
-                    loadNextSetOfArticles(page);
-                    searchArticles();
-                }
-            };
-            // Adds the scroll listener to RecyclerView
-            rvArticles.addOnScrollListener(scrollListener);
-        }
+        // Attach the layout manager to the recycler view
+        rvArticles.setLayoutManager(staggeredGridLayoutManager);
+        articlesAdapter = new ArticlesAdapter(allArticles);
+        rvArticles.setAdapter(articlesAdapter);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(staggeredGridLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                loadNextSetOfArticles(page);
+                //searchArticles();
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        rvArticles.addOnScrollListener(scrollListener);
+
     }
 
     // Menu icons are inflated just as they were with actionbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            //Log ("SAMY", "Searching articles");
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // perform query here
+                searchArticles(query);
+                Toast.makeText(ListActivity.this, "Searching! " + query, Toast.LENGTH_SHORT).show();
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
-    // Define the callback when ActionMode is activated
-    private ActionMode.Callback modeCallBack = new ActionMode.Callback() {
-        // Called when the action mode is created; startActionMode() was called
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            mode.setTitle("Actions");
-            mode.getMenuInflater().inflate(R.menu.menu_main, menu);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_filter) {
+            Toast.makeText(this, "Filtering clicked", Toast.LENGTH_LONG).show();
+        }
+
+        if (id == R.id.action_search) {
+            Toast.makeText(this, "Search clicked", Toast.LENGTH_LONG).show();
             return true;
         }
-
-        // Called each time the action mode is shown.
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            return false; // Return false if nothing is done
-        }
-
-        // Called when the user selects a contextual menu item
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.action_search:
-                    Toast.makeText(ListActivity.this, "Searching!", Toast.LENGTH_SHORT).show();
-                    mode.finish(); // Action picked, so close the contextual menu
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        // Called when the user exits the action mode
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-            currentActionMode = null; // Clear current action mode
-        }
-    };
+        return super.onOptionsItemSelected(item);
+    }
 
     public void loadNextSetOfArticles(int pageNum) {
         OkHttpClient client = new OkHttpClient();
@@ -150,7 +150,7 @@ public class ListActivity extends AppCompatActivity {
             public void onResponse(Call call, final Response response) throws IOException {
                 Log.i("SAMY", "response");
                 if (!response.isSuccessful()) {
-                    Log.i("SAMY", "unsuccessful"+response.toString());
+                    Log.i("SAMY", "unsuccessful" + response.toString());
                     throw new IOException("Unexpected code " + response);
                 } else {
                     Log.i("SAMY", "successful");
@@ -175,15 +175,12 @@ public class ListActivity extends AppCompatActivity {
         });
     }
 
-    public void onClickSearch(View view) {
-        searchArticles();
-    }
+    private void searchArticles(String query) {
 
-    private void searchArticles() {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=054c6d1972c142b4a8c84bbb300c4d87")
+                .url("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=054c6d1972c142b4a8c84bbb300c4d87" + "&q=" + query)
                 .build();
         Log.i("SAMY", "Searching articles");
         client.newCall(request).enqueue(new Callback() {
